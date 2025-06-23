b51422d3a22d5c5860bd5ae7d8f6145cb19ede12
Oluwadare Seyi <oluwadareseyii@gmail.com>
2025-06-23 11:18:08 +0100

    chore: add ignore config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
